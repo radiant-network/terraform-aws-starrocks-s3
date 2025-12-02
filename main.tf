5360da47cb89918fc3a6b401f355555c0ca24866
Neel Kuila <neelk1123@gmail.com>
2025-12-02 11:09:38 -0500

    merge frontend blocks, remove compute node block

--- main.tf
+++ main.tf
@@ -8,6 +8,13 @@ data "aws_vpc" "target_vpc" {
   id = var.vpc_id
 }
 
+locals {
+  # When upgrading StarRocks, new FE and CN nodes should use the new version 
+  # to test compatibility. Otherwise use the same version as the leader
+  canary_version = var.star_rocks_upgrade_version != "" ? var.star_rocks_upgrade_version : var.star_rocks_version
+  canary_compute_node_count = var.star_rocks_upgrade_version != "" ? var.compute_node_instance_count + 1 : var.compute_node_instance_count
+}
+
 # Manually controlled leader IP node for upgrades
 resource "aws_ssm_parameter" "leader_ip" {
   name  = "${var.project}-${var.environment}-leader-ip"
@@ -21,52 +28,12 @@ resource "aws_ssm_parameter" "leader_ip" {
   }
 }
 
-
-resource "aws_instance" "star_rocks_compute_nodes" {
-  count         = var.compute_node_instance_count
-  ami           = var.ami_id
-  instance_type = var.compute_node_instance_type
-  user_data = templatefile("${path.module}/templates/compute_node_startup.sh.tpl", {
-    starrocks_version   = var.star_rocks_version
-    starrocks_data_path = var.starrocks_data_path
-    fe_host             = aws_route53_record.private_star_rocks_dns.fqdn
-    fe_query_port       = 9030
-    vpc_cidr            = data.aws_vpc.target_vpc.cidr_block
-    java_heap_size_mb   = var.compute_node_heap_size
-  })
-  iam_instance_profile   = aws_iam_instance_profile.star_rocks_instance_profile.name
-  vpc_security_group_ids = [aws_security_group.star_rocks_sg.id]
-  subnet_id              = var.subnet_id
-  key_name               = var.ssh_key_name
-  ebs_optimized          = true
-  monitoring             = true
-  volume_tags = {
-    Name             = "${var.project}-cn-${var.environment}-volume"
-    Application      = var.project
-    Description      = "Instance for ${var.project}"
-    Starrocks_Backup = "true"
-  }
-  root_block_device {
-    volume_type = "standard"
-    volume_size = var.cn_volume_size_gb
-    encrypted   = "true"
-  }
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens   = "required"
-  }
-  tags = {
-    Name             = "${var.project}-cn-${var.environment}"
-    Application      = "${var.project}-cn"
-    Description      = "Instance for ${var.project}"
-    Starrocks_Backup = "true"
-  }
-}
 resource "aws_instance" "star_rocks_frontend" {
+  count         = var.frontend_instance_count
   ami           = var.ami_id
   instance_type = var.frontend_instance_type
   user_data = templatefile("${path.module}/templates/frontend_startup.sh.tpl", {
-    starrocks_version   = var.star_rocks_version
+    starrocks_version   = count.index == 0 ? var.star_rocks_version : local.canary_version
     starrocks_data_path = var.starrocks_data_path
     region              = var.region
     bucket              = "${var.starrocks_bucket}"
@@ -85,6 +52,7 @@ resource "aws_instance" "star_rocks_frontend" {
     Name             = "${var.project}-fe-${var.environment}-volume"
     Application      = var.project
     Description      = "Instance for ${var.project}"
+    Version          = count.index == 0 ? var.star_rocks_version : local.canary_version
     Starrocks_Backup = "true"
   }
   root_block_device {
@@ -97,93 +65,22 @@ resource "aws_instance" "star_rocks_frontend" {
     http_tokens   = "required"
   }
   tags = {
-    Name             = "${var.project}-fe-${var.environment}-1"
+    Name             = "${var.project}-fe-${var.environment}-${count.index + 1}" # +1 because humans like 1-indexing
     Application      = "${var.project}-fe"
     Description      = "Instance for ${var.project}"
+    Version          = count.index == 0 ? var.star_rocks_version : local.canary_version
     Starrocks_Backup = "true"
   }
 }
 
-resource "aws_instance" "star_rocks_grafana" {
+# During an upgrade, the number of compute nodes is increased by one, and the
+# new compute node will use the new version to verify compatibility
+resource "aws_instance" "star_rocks_compute_nodes" {
+  count         = local.canary_compute_node_count
   ami           = var.ami_id
-  instance_type = var.monitoring_instance_type
-  user_data = templatefile("${path.module}/templates/grafana_startup.sh.tpl", {
-    prometheus_ip = aws_instance.star_rocks_prometheus.private_ip
-    bucket        = "${var.starrocks_bucket}"
-    pw_secret     = "${var.project}-${var.environment}-grafana-admin-pw"
-  })
-  iam_instance_profile   = aws_iam_instance_profile.monitoring_instance_profile.name
-  vpc_security_group_ids = [aws_security_group.grafana_sg.id]
-  subnet_id              = var.subnet_id
-  key_name               = var.ssh_key_name
-  ebs_optimized          = true
-  monitoring             = true
-  volume_tags = {
-    Name             = "${var.project}-grafana-${var.environment}-volume"
-    Application      = var.project
-    Description      = "Instance for ${var.project}"
-    Starrocks_Backup = "false"
-  }
-  root_block_device {
-    volume_type = "standard"
-    volume_size = var.root_volume_size_gb
-    encrypted   = "true"
-  }
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens   = "required"
-  }
-  tags = {
-    Name             = "${var.project}-grafana-${var.environment}"
-    Application      = var.project
-    Description      = "Instance for ${var.project}"
-    Starrocks_Backup = "false"
-  }
-}
-
-
-resource "aws_instance" "star_rocks_prometheus" {
-  ami           = var.ami_id
-  instance_type = var.monitoring_instance_type
-  user_data = templatefile("${path.module}/templates/prometheus_startup.sh.tpl", {
-    cn_tag = "${var.project}-cn"
-    fe_tag = "${var.project}-fe"
-  })
-  iam_instance_profile   = aws_iam_instance_profile.monitoring_instance_profile.name
-  vpc_security_group_ids = [aws_security_group.prometheus_sg.id]
-  subnet_id              = var.subnet_id
-  key_name               = var.ssh_key_name
-  ebs_optimized          = true
-  monitoring             = true
-  volume_tags = {
-    Name             = "${var.project}-${var.environment}-volume"
-    Application      = var.project
-    Description      = "Instance for ${var.project}"
-    Starrocks_Backup = "false"
-  }
-  root_block_device {
-    volume_type = "standard"
-    volume_size = var.root_volume_size_gb
-    encrypted   = "true"
-  }
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens   = "required"
-  }
-  tags = {
-    Name             = "${var.project}-prometheus-${var.environment}"
-    Application      = var.project
-    Description      = "Instance for ${var.project}"
-    Starrocks_Backup = "false"
-  }
-}
-
-resource "aws_instance" "upgrade_compute_nodes" {
-  count         = var.star_rocks_upgrade_version != "" ? 1 : 0
-  ami           = var.ami_id
-  instance_type = var.monitoring_instance_type
+  instance_type = var.compute_node_instance_type
   user_data = templatefile("${path.module}/templates/compute_node_startup.sh.tpl", {
-    starrocks_version   = var.star_rocks_upgrade_version
+    starrocks_version   = count.index == (local.canary_compute_node_count - 1) ? var.star_rocks_version : local.canary_version
     starrocks_data_path = var.starrocks_data_path
     fe_host             = aws_route53_record.private_star_rocks_dns.fqdn
     fe_query_port       = 9030
@@ -200,6 +97,7 @@ resource "aws_instance" "upgrade_compute_nodes" {
     Name             = "${var.project}-cn-${var.environment}-volume"
     Application      = var.project
     Description      = "Instance for ${var.project}"
+    Version          = count.index == (local.canary_compute_node_count - 1) ? var.star_rocks_version : local.canary_version
     Starrocks_Backup = "true"
   }
   root_block_device {
@@ -212,9 +110,10 @@ resource "aws_instance" "upgrade_compute_nodes" {
     http_tokens   = "required"
   }
   tags = {
-    Name             = "${var.project}-cn-${var.environment}-upgrade"
+    Name             = "${var.project}-cn-${var.environment}"
     Application      = "${var.project}-cn"
     Description      = "Instance for ${var.project}"
+    Version          = count.index == (local.canary_compute_node_count - 1) ? var.star_rocks_version : local.canary_version
     Starrocks_Backup = "true"
   }
 }
