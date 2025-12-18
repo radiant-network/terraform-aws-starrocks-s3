13500412d6825e2908d877177c3f41669dfb9957
jecos <jcostanza@ferlab.bio>
2025-12-18 16:08:13 -0500

    :sparkles: output role arn

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "fe_dns_name" {
 output "grafana_address" {
   value = aws_instance.star_rocks_grafana.private_ip
 }
+
+output "star_rocks_role_arn" {
+  value = aws_iam_role.star_rocks_role.arn
+}
