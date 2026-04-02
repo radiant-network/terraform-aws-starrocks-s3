54261b856fc4ceb0b346903c669d1c4f513080f2
Neel Kuila <neelk1123@gmail.com>
2026-04-02 12:14:43 -0400

    update type

--- variables.tf
+++ variables.tf
@@ -103,9 +103,19 @@ variable "additional_cn_user_data" {
 }
 
 variable "additional_ingress_rules" {
-  default = ""
+  description = "Security group rules to add for ingress"
+  type = list(object({
+    port        = number
+    cidr_blocks = list(string)
+  }))
+  default = []
 }
 
 variable "additional_egress_rules" {
-  default = ""
+  description = "Security group rules to add for egress"
+  type = list(object({
+    port        = number
+    cidr_blocks = list(string)
+  }))
+  default = []
 }
